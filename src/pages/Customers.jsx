61bced192672e0e8a58f790398d93e8286ea7d2e
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-20 16:00:04 +0530

    user delete

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -20,15 +20,19 @@ const Customers = () => {
       Header: "EMAIL",
       accessor: "email",
     },
+
     {
       Header: "ACTIONS",
       Cell: ({ row }) => (
         <div className="flex justify-between w-40 ">
-          <Link to={`/edit-product/${row.original._id}`}>
-            <button className="bg-slate-700 text-white w-16 py-1 rounded-lg font-bold italic shadow hover:bg-black hover:border border-borderColor">
-              EDIT
-            </button>
-          </Link>
+          <button
+            onClick={() => toggleBlockStatus(row.original._id)}
+            className={`bg-${
+              row.original.allow === "block" ? "red" : "green"
+            }-500 text-white w-20 py-1 rounded-lg font-bold italic shadow hover:bg-black hover:border border-borderColor`}
+          >
+            {row.original.allow === "block" ? "UNBLOCK" : "BLOCK"}
+          </button>
           <button
             onClick={() => handleDelete(row.original._id)}
             className="bg-red-500 text-white w-16 rounded-lg font-bold italic shadow hover:bg-red-700"
@@ -40,18 +44,63 @@ const Customers = () => {
     },
   ];
 
-  useEffect(() => {
-    const fetchAllCustomers = async () => {
-      try {
-        const response = await api.get("/profile/user-profile/get");
-        setCustomersData(response.data.customers);
-      } catch (error) {
-        toast.error("Failed to fetch all customers data");
-        console.log("Failed to fetch all customers data");
+  const toggleBlockStatus = async (userId) => {
+    try {
+      const response = await api.post(`/profile/user/block-unblock/${userId}`);
+      const updatedCustomersData = customersData.map((customer) => {
+        if (customer._id === userId) {
+          // Toggle the allow status of the user
+          return {
+            ...customer,
+            allow: response.data.message.includes("unblock") ? "unblock" : "block",
+          };
+        }
+        return customer;
+      });
+      setCustomersData(updatedCustomersData);
+      toast.success(
+        `User ${
+          response.data.message.includes("unblock") ? "unblock" : "block"
+        }ed successfully`
+      );
+    } catch (error) {
+      toast.error("Failed to toggle user's block status");
+      console.error("Failed to toggle user's block status:", error);
+    }
+  };
+
+  const handleDelete = async (userId) => {
+    try {
+
+      const response = await api.post(`/profile/delete-user/${userId}`);
+      if (response.status === 200) {
+        // Filter out the deleted user from the customers data
+        const updatedCustomersData = customersData.filter((customer) => customer._id !== userId);
+        setCustomersData(updatedCustomersData);
+        toast.success("User deleted successfully");
+      } else {
+        toast.error("Failed to delete user");
       }
-    };
+    } catch (error) {
+      toast.error("Failed to delete user");
+      console.error("Failed to delete user:", error);
+    }
+  };
+
+  const fetchAllCustomers = async () => {
+    try {
+      const response = await api.get("/profile/user-profile/get");
+      setCustomersData(response.data.customers);
+    } catch (error) {
+      toast.error("Failed to fetch all customers data");
+      console.log("Failed to fetch all customers data");
+    }
+  };
+
+  useEffect(() => {
     fetchAllCustomers();
   }, []);
+
   return (
     <div>
       <BasicTable columnsProps={CUSTOMERS} dataProps={customersData} />
